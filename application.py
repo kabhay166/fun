c8a3576c84deb6d8331359b6cb19b845113a8f1a
Abhay Kumar <kabhay166@gmail.com>
2020-09-07 14:42:36 +0530

    changed the template folder and modified the application file

--- application.py
+++ application.py
@@ -6,14 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("test.html")
-
-@app.route("/Abhay")
-def abhay():
-    return redirect("http://kabhay166.github.io", code=302)
+    return render_template("index.html")
 
 @app.route("/Abhishek")
 def abhishek():
     return "<h1>Abhishek is a very good friend of author</h1><ul><li>He is lovely</li><li>He also loves phyics.</li><li>He is currently studying in IIT.</li></ul>"
+@app.route("/test")
+
 
 

06cba663cd8f1ee0b1bfd8e990fc828145cc41cb
Abhay Kumar <kabhay166@gmail.com>
2020-09-15 13:00:41 +0530

    added route for the other pages and modified the content of the cosmos page

--- application.py
+++ application.py
@@ -6,3 +6,19 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/home.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/ascent-of-man.html")
+def ascentOfMan():
+    return render_template("ascent-of-man.html")
+
+@app.route("/cosmos.html")
+def cosmos():
+    return render_template("cosmos.html")
